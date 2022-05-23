ede24376b08d046b967924d18c0cd2b863da62c3
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-05-23 11:14:59 -0600

    Comentarios en servicio

--- FETarjetaCredito/src/app/services/tarjeta.service.ts
+++ FETarjetaCredito/src/app/services/tarjeta.service.ts
@@ -11,20 +11,24 @@ export class TarjetaService {
 
   constructor(private http: HttpClient) {}
 
-  getListTarjetas(): Observable<any>{
+  getListTarjetas(): Observable<any>{ //metodo para traer la lista de Tarjetas
     return this.http.get(this.myAppUrl + this.myApiUrl);
    }
 
 
-   deleteTarjeta(id: number):Observable<any>{
+   deleteTarjeta(id: number):Observable<any>{ //metodo para eliminar una unica tarjeta por cada tiro
      return this.http.delete(this.myAppUrl + this.myApiUrl + id);
    }
 
-   saveTarjeta(tarjeta:any): Observable<any>{
+   saveTarjeta(tarjeta:any): Observable<any>{ //metodo para agregar y guardar una nueva tarjeta en el array o BD
      return this.http.post(this.myAppUrl + this.myApiUrl, tarjeta);
    }
 
-   updateTarjeta(id:number, tarjeta: any): Observable<any>{
+   updateTarjeta(id:number, tarjeta: any): Observable<any>{ //Metodo para actualizar una tarjeta
      return this.http.put(this.myAppUrl + this.myApiUrl + id, tarjeta);
    }
 }
+ //Un observable puede ser creado a partir de eventos de usuario derivados del uso de un formulario, una llamada HTTP,
+ //un almacén de datos, etc.
+ //Mediante el observable nos podemos suscribir a eventos que nos permiten hacer cosas cuando cambia lo que se esté observando.
+ //Observer: Es el actor que se dedica a observar.
